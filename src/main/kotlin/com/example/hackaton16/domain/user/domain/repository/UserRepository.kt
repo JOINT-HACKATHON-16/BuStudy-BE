10d2cb97c71c42ef9f82492ace192b6d7426da52
coehgns <modooboiroo@gmail.com>
2025-11-05 20:11:18 +0900

    feat: UserRepository에 findByLoginId 추가

--- src/main/kotlin/com/example/hackaton16/domain/user/domain/repository/UserRepository.kt
+++ src/main/kotlin/com/example/hackaton16/domain/user/domain/repository/UserRepository.kt
@@ -4,4 +4,6 @@ import com.example.hackaton16.domain.user.domain.User
 import org.springframework.data.jpa.repository.JpaRepository
 
 interface UserRepository : JpaRepository<User, Long> {
+
+    fun findByLoginId(loginId: String): User?
 }
