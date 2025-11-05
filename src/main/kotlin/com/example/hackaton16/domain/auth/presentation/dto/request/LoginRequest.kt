ae45a3e5bb4c975717f11a8c2cff961de03cb5ab
coehgns <modooboiroo@gmail.com>
2025-11-05 22:19:06 +0900

    feat: LoginRequest

---
+++ src/main/kotlin/com/example/hackaton16/domain/auth/presentation/dto/request/LoginRequest.kt
@@ -0,0 +1,16 @@
+package com.example.hackaton16.domain.auth.presentation.dto.request
+
+import jakarta.validation.constraints.NotBlank
+import jakarta.validation.constraints.Pattern
+
+data class LoginRequest(
+    @field:NotBlank(message = "아이디는 필수 입력입니다.")
+    val loginId: String,
+
+    @field:NotBlank(message = "비밀번호는 필수 입력입니다.")
+    @field:Pattern(
+        regexp = "^(?=.*[A-Za-z])(?=.*\\d)(?=.*[!@#$%^&*()_\\-+=\\[\\]{}|;:'\\\",.<>/?])[A-Za-z\\d!@#$%^&*()_\\-+=\\[\\]{}|;:'\\\",.<>/?]{8,20}$",
+        message = "비밀번호는 영문, 숫자, 특수문자를 포함해 8~20자여야 합니다."
+    )
+    val password: String
+)
