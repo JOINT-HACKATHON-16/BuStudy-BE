00af3ba7ae103de2c35b1e33b48702f51cc0d8db
coehgns <modooboiroo@gmail.com>
2025-11-05 21:54:40 +0900

    feat: SignUpRequest name 필드 추가

--- src/main/kotlin/com/example/hackaton16/domain/auth/presentation/dto/request/SignUpRequest.kt
+++ src/main/kotlin/com/example/hackaton16/domain/auth/presentation/dto/request/SignUpRequest.kt
@@ -5,6 +5,8 @@ import jakarta.validation.constraints.Pattern
 data class SignUpRequest(
     val loginId: String,
 
+    val name: String,
+
     @field:Pattern(
         regexp = "^(?=.*[A-Za-z])(?=.*\\d)(?=.*[!@#$%^&*()_\\-+=\\[\\]{}|;:'\\\",.<>/?])[A-Za-z\\d!@#$%^&*()_\\-+=\\[\\]{}|;:'\\\",.<>/?]{8,20}$",
         message = "비밀번호는 영문, 숫자, 특수문자를 포함해 8~20자여야 합니다."
