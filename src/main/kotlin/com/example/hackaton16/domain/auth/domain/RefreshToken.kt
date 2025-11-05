e7737bb22a7207c9b92192061f186a63dd790e04
coehgns <modooboiroo@gmail.com>
2025-11-05 20:16:06 +0900

    feat: RefreshToken

---
+++ src/main/kotlin/com/example/hackaton16/domain/auth/domain/RefreshToken.kt
@@ -0,0 +1,18 @@
+package com.example.hackaton16.domain.auth.domain
+
+import jakarta.persistence.Id
+import org.springframework.data.redis.core.RedisHash
+import org.springframework.data.redis.core.TimeToLive
+import org.springframework.data.redis.core.index.Indexed
+
+@RedisHash
+class RefreshToken(
+    @Id
+    val id: String,
+
+    @Indexed
+    var token: String,
+
+    @TimeToLive
+    var ttl: Long
+)
