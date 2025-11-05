952aebd4babe96e53cc7dc90c35d3eddf69337de
coehgns <modooboiroo@gmail.com>
2025-11-05 21:49:11 +0900

    feat: SignUpRequest

---
+++ src/main/kotlin/com/example/hackaton16/domain/auth/presentation/dto/request/SignUpRequest.kt
@@ -0,0 +1,15 @@
+package com.example.hackaton16.domain.auth.presentation.dto.request
+
+import jakarta.validation.constraints.Pattern
+
+data class SignUpRequest(
+    val loginId: String,
+
+    @field:Pattern(
+        regexp = "^(?=.*[A-Za-z])(?=.*\\d)(?=.*[!@#$%^&*()_\\-+=\\[\\]{}|;:'\\\",.<>/?])[A-Za-z\\d!@#$%^&*()_\\-+=\\[\\]{}|;:'\\\",.<>/?]{8,20}$",
+        message = "비밀번호는 영문, 숫자, 특수문자를 포함해 8~20자여야 합니다."
+    )
+    val password: String,
+
+    val passwordCheck: String
+)
