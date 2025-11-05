cb899db0acd2c7a47722d505b768c36c9fca21dd
coehgns <modooboiroo@gmail.com>
2025-11-05 19:52:24 +0900

    feat: User

---
+++ src/main/kotlin/com/example/hackaton16/domain/user/domain/User.kt
@@ -0,0 +1,23 @@
+package com.example.hackaton16.domain.user.domain
+
+import jakarta.persistence.Column
+import jakarta.persistence.Entity
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.GenerationType
+import jakarta.persistence.Id
+
+@Entity(name = "tbl_user")
+class User(
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private val id: Long,
+
+    @Column(name = "login_id", nullable = false, unique = true)
+    private val loingId: String,
+
+    @Column(name = "name", nullable = false)
+    private val name: String,
+
+    @Column(name = "password", nullable = false)
+    private val password: String
+)
