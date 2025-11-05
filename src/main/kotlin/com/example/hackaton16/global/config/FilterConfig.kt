26b1e300f5271b55f6f4c894b9a05fb87e205b7e
coehgns <modooboiroo@gmail.com>
2025-11-05 21:40:12 +0900

    feat: FilterConfig

---
+++ src/main/kotlin/com/example/hackaton16/global/config/FilterConfig.kt
@@ -0,0 +1,24 @@
+package com.example.hackaton16.global.config
+
+import com.example.hackaton16.global.error.GlobalExceptionFilter
+import com.example.hackaton16.global.security.jwt.JwtFilter
+import com.example.hackaton16.global.security.jwt.JwtTokenProvider
+import com.fasterxml.jackson.databind.ObjectMapper
+import org.springframework.security.config.annotation.SecurityConfigurerAdapter
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.web.DefaultSecurityFilterChain
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter
+
+class FilterConfig(
+    private val jwtTokenProvider: JwtTokenProvider,
+    private val objectMapper: ObjectMapper
+) : SecurityConfigurerAdapter<DefaultSecurityFilterChain, HttpSecurity>() {
+
+    override fun configure(http: HttpSecurity) {
+        val jwtTokenFilter = JwtFilter(jwtTokenProvider)
+        val globalExceptionFilter = GlobalExceptionFilter(objectMapper)
+
+        http.addFilterBefore(jwtTokenFilter, UsernamePasswordAuthenticationFilter::class.java)
+            .addFilterBefore(globalExceptionFilter, JwtFilter::class.java)
+    }
+}
