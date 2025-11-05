2768c5a5a4fe8aaef731232f5758dee993151103
coehgns <modooboiroo@gmail.com>
2025-11-05 22:59:02 +0900

    feat: AuthController에 AuthApiDocument 적용

--- src/main/kotlin/com/example/hackaton16/domain/auth/presentation/AuthController.kt
+++ src/main/kotlin/com/example/hackaton16/domain/auth/presentation/AuthController.kt
@@ -4,6 +4,7 @@ import com.example.hackaton16.domain.auth.presentation.dto.request.LoginRequest
 import com.example.hackaton16.domain.auth.presentation.dto.request.SignUpRequest
 import com.example.hackaton16.domain.auth.service.LoginService
 import com.example.hackaton16.domain.auth.service.SignUpService
+import com.example.hackaton16.global.document.auth.AuthApiDocument
 import com.example.hackaton16.global.utils.token.dto.TokenResponse
 import jakarta.validation.Valid
 import org.springframework.http.HttpStatus
@@ -18,17 +19,17 @@ import org.springframework.web.bind.annotation.RestController
 class AuthController(
     private val signUpService: SignUpService,
     private val loginService: LoginService
-) {
+) : AuthApiDocument {
 
     @PostMapping("/sign-up")
     @ResponseStatus(HttpStatus.CREATED)
-    fun signUp(@RequestBody @Valid request: SignUpRequest) {
+    override fun signUp(@RequestBody @Valid request: SignUpRequest) {
         signUpService.execute(request)
     }
 
     @PostMapping("/login")
     @ResponseStatus(HttpStatus.OK)
-    fun login(@RequestBody @Valid request: LoginRequest): TokenResponse {
+    override fun login(@RequestBody @Valid request: LoginRequest): TokenResponse {
         return loginService.execute(request)
     }
 }
