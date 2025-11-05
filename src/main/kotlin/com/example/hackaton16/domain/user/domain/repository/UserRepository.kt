b171333998962d63b2c4b5020efc16e6303ac884
coehgns <modooboiroo@gmail.com>
2025-11-05 19:52:29 +0900

    feat: UserRepository

---
+++ src/main/kotlin/com/example/hackaton16/domain/user/domain/repository/UserRepository.kt
@@ -0,0 +1,7 @@
+package com.example.hackaton16.domain.user.domain.repository
+
+import com.example.hackaton16.domain.user.domain.User
+import org.springframework.data.jpa.repository.JpaRepository
+
+interface UserRepository : JpaRepository<User, Long> {
+}
