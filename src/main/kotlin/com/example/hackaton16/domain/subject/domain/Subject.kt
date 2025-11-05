0b98bbbaf1c08ad718d8ef18ba5f399b28ecb0fe
coehgns <modooboiroo@gmail.com>
2025-11-05 23:13:02 +0900

    feat: Subject

---
+++ src/main/kotlin/com/example/hackaton16/domain/subject/domain/Subject.kt
@@ -0,0 +1,25 @@
+package com.example.hackaton16.domain.subject.domain
+
+import com.example.hackaton16.domain.user.domain.User
+import jakarta.persistence.Column
+import jakarta.persistence.Entity
+import jakarta.persistence.FetchType
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.GenerationType
+import jakarta.persistence.Id
+import jakarta.persistence.JoinColumn
+import jakarta.persistence.ManyToOne
+
+@Entity(name = "tbl_subject")
+class Subject(
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    val id: Long = 0,
+
+    @Column(name = "subject", nullable = false)
+    val subject: String,
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    val user: User
+)
