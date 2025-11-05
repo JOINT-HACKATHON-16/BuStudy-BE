98003a39e980503d2b71108da8b08af462c50d14
coehgns <modooboiroo@gmail.com>
2025-11-05 20:25:08 +0900

    feat: ErrorCode에 EXPIRED_TOKEN 추가

--- src/main/kotlin/com/example/hackaton16/global/error/exception/ErrorCode.kt
+++ src/main/kotlin/com/example/hackaton16/global/error/exception/ErrorCode.kt
@@ -5,6 +5,7 @@ enum class ErrorCode(
     val message: String,
 ) {
     INVALID_TOKEN(401, "Invalid Token"),
+    EXPIRED_TOKEN(401, "Expired Token"),
 
     USER_NOT_FOUND(404, "User Not Found"),
     INTERNAL_SERVER_ERROR(500, "Internal Server Error"),
