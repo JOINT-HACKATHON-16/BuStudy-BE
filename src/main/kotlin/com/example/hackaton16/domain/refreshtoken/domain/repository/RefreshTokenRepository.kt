c5854b5d5bdaff43b4d212be18913b7a1f99f548
coehgns <modooboiroo@gmail.com>
2025-11-05 21:09:49 +0900

    chore: RefreshTokenRepository findByToken 메서드 RefreshToken nullable하게 수정

--- src/main/kotlin/com/example/hackaton16/domain/refreshtoken/domain/repository/RefreshTokenRepository.kt
+++ src/main/kotlin/com/example/hackaton16/domain/refreshtoken/domain/repository/RefreshTokenRepository.kt
@@ -4,4 +4,6 @@ import com.example.hackaton16.domain.refreshtoken.domain.RefreshToken
 import org.springframework.data.repository.CrudRepository
 
 interface RefreshTokenRepository : CrudRepository<RefreshToken, String> {
+
+    fun findByToken(refreshToken: String): RefreshToken?
 }
