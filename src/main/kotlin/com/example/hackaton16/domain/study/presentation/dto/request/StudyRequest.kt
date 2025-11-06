bd14b4782126f3f488aca9cfb1fe8e63ca2abdd8
coehgns <modooboiroo@gmail.com>
2025-11-06 15:47:23 +0900

    refactor: StudyRequest 필드 수정

--- src/main/kotlin/com/example/hackaton16/domain/study/presentation/dto/request/StudyRequest.kt
+++ src/main/kotlin/com/example/hackaton16/domain/study/presentation/dto/request/StudyRequest.kt
@@ -1,6 +1,9 @@
 package com.example.hackaton16.domain.study.presentation.dto.request
 
+import jakarta.validation.constraints.Size
+
 data class StudyRequest(
-    val numQuestions: Int,
-    val difficulty: String
+
+    @field:Size(min = 1, message = "예상 시간은 최소 1분 이상이여야 합니다.")
+    val estimatedTime: Int
 )
