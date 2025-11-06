e51a09985f37d52f55957fa795f83658985a0d08
coehgns <modooboiroo@gmail.com>
2025-11-06 14:09:27 +0900

    feat: BusStopController에 BusStopApiDocument 적용

--- src/main/kotlin/com/example/hackaton16/domain/bus/presentation/BusStopController.kt
+++ src/main/kotlin/com/example/hackaton16/domain/bus/presentation/BusStopController.kt
@@ -1,6 +1,7 @@
 package com.example.hackaton16.domain.bus.presentation
 
 import com.example.hackaton16.domain.bus.service.QueryBusStopListService
+import com.example.hackaton16.global.document.bus.BusApiDocument
 import com.example.hackaton16.infrastructure.feign.client.datago.dto.response.BusStopResponse
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
@@ -11,10 +12,10 @@ import org.springframework.web.bind.annotation.RestController
 @RequestMapping("/bus")
 class BusStopController(
     private val queryBusStopListService: QueryBusStopListService
-) {
+) : BusApiDocument {
 
     @GetMapping
-    fun queryBusStopList(
+    override fun queryBusStopList(
         @RequestParam lat: Double,
         @RequestParam lon: Double
     ): List<BusStopResponse.BusStationItem> {
