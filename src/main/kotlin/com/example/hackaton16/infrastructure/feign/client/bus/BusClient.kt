4643e285dc8f1825a20b2ec78b80d0048022d820
coehgns <modooboiroo@gmail.com>
2025-11-06 23:41:37 +0900

    feat: BusClient

---
+++ src/main/kotlin/com/example/hackaton16/infrastructure/feign/client/bus/BusClient.kt
@@ -0,0 +1,15 @@
+package com.example.hackaton16.infrastructure.feign.client.bus
+
+import com.example.hackaton16.domain.bus.presentation.dto.response.GenerateEstimatedTimeResponse
+import com.example.hackaton16.infrastructure.feign.client.bus.dto.request.BusEstimatedTimeRequest
+import org.springframework.cloud.openfeign.FeignClient
+import org.springframework.web.bind.annotation.PostMapping
+
+@FeignClient(
+    name = "BusClient",
+    url = ""
+)
+interface BusClient {
+    @PostMapping("/bus")
+    fun generateEstimatedTime(request: BusEstimatedTimeRequest): GenerateEstimatedTimeResponse
+}
