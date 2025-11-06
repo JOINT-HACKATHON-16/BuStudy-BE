696b53f17f440b17fccea1d10848d7907c730b32
coehgns <modooboiroo@gmail.com>
2025-11-06 15:48:48 +0900

    feat: FastApiClient에 generateAudio 메서드 추가

--- src/main/kotlin/com/example/hackaton16/infrastructure/feign/client/fastapi/FastApiClient.kt
+++ src/main/kotlin/com/example/hackaton16/infrastructure/feign/client/fastapi/FastApiClient.kt
@@ -1,14 +1,17 @@
 package com.example.hackaton16.infrastructure.feign.client.fastapi
 
 import com.example.hackaton16.infrastructure.feign.client.fastapi.dto.request.GenerateQuizRequest
+import com.example.hackaton16.infrastructure.feign.client.fastapi.dto.response.AudioResponse
 import com.example.hackaton16.infrastructure.feign.client.fastapi.dto.response.GenerateQuizResponse
 import com.example.hackaton16.infrastructure.feign.client.fastapi.dto.response.RecommendSubjectResponse
 import org.springframework.cloud.openfeign.FeignClient
 import org.springframework.http.MediaType
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestPart
 import org.springframework.web.multipart.MultipartFile
+import java.util.UUID
 
 @FeignClient(
     name = "SubjectClient",
@@ -20,4 +23,7 @@ interface FastApiClient {
 
     @PostMapping("/rag/quiz")
     fun generateQuiz(@RequestBody request: GenerateQuizRequest): GenerateQuizResponse
+
+    @PostMapping("/rag/review/{upload-id}")
+    fun generateAudio(@PathVariable("upload-id") uploadId: UUID): AudioResponse
 }
