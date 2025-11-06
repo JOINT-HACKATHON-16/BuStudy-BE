919141efa1327133fa029c0c72859a94008b05d1
coehgns <modooboiroo@gmail.com>
2025-11-06 21:50:58 +0900

    refactor: QueryBusStopListService

--- src/main/kotlin/com/example/hackaton16/domain/bus/service/QueryBusStopListService.kt
+++ src/main/kotlin/com/example/hackaton16/domain/bus/service/QueryBusStopListService.kt
@@ -1,7 +1,7 @@
 package com.example.hackaton16.domain.bus.service
 
+import com.example.hackaton16.domain.bus.presentation.dto.response.BusStopListResponse
 import com.example.hackaton16.infrastructure.feign.client.datago.DataGoClient
-import com.example.hackaton16.infrastructure.feign.client.datago.dto.response.BusStopResponse
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Service
 
@@ -13,8 +13,17 @@ class QueryBusStopListService(
     @Value("\${app.data-go.service-key}")
     lateinit var serviceKey: String
 
-    fun execute(lat: Double, lon: Double): List<BusStopResponse.BusStationItem> {
-        val response = dataGoClient.getBusStopList(serviceKey, lat, lon)
-        return response.response?.body?.items?.item.orEmpty()
+    fun execute(lat: Double, lon: Double): BusStopListResponse {
+        val responseList = dataGoClient.getBusStopList(serviceKey, lat, lon)
+        return BusStopListResponse(
+            responseList.response.body.items.item.map {
+                BusStopListResponse.BusStopDto(
+                    it.nodeid,
+                    it.gpslati,
+                    it.gpslong,
+                    it.nodenm
+                )
+            }
+        )
     }
 }
