ca138a86f46a09dc31d35b7fd656237150235b69
coehgns <modooboiroo@gmail.com>
2025-11-06 10:53:54 +0900

    feat: AuthController에 tokenRefresh 메서드 추가

--- src/main/kotlin/com/example/hackaton16/domain/auth/presentation/AuthController.kt
+++ src/main/kotlin/com/example/hackaton16/domain/auth/presentation/AuthController.kt
@@ -4,12 +4,15 @@ import com.example.hackaton16.domain.auth.presentation.dto.request.LoginRequest
 import com.example.hackaton16.domain.auth.presentation.dto.request.SignUpRequest
 import com.example.hackaton16.domain.auth.service.LoginService
 import com.example.hackaton16.domain.auth.service.SignUpService
+import com.example.hackaton16.domain.auth.service.TokenRefreshService
 import com.example.hackaton16.global.document.auth.AuthApiDocument
 import com.example.hackaton16.global.utils.token.dto.TokenResponse
 import jakarta.validation.Valid
 import org.springframework.http.HttpStatus
 import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.PutMapping
 import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestHeader
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.ResponseStatus
 import org.springframework.web.bind.annotation.RestController
@@ -18,7 +21,8 @@ import org.springframework.web.bind.annotation.RestController
 @RequestMapping("/auth")
 class AuthController(
     private val signUpService: SignUpService,
-    private val loginService: LoginService
+    private val loginService: LoginService,
+    private val tokenRefreshService: TokenRefreshService
 ) : AuthApiDocument {
 
     @PostMapping("/sign-up")
@@ -31,5 +35,11 @@ class AuthController(
     @ResponseStatus(HttpStatus.OK)
     override fun login(@RequestBody @Valid request: LoginRequest): TokenResponse {
         return loginService.execute(request)
+    }
+
+    @PutMapping("/re-issue")
+    @ResponseStatus(HttpStatus.OK)
+    fun tokenRefresh(@RequestHeader("X-Refresh-Token") refreshToken: String): TokenResponse {
+        return tokenRefreshService.execute(refreshToken)
     }
 }
