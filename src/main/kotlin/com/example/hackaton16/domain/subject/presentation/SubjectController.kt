427e6ef5de8f20ad9f75ccf48e365ca3d1011392
coehgns <modooboiroo@gmail.com>
2025-11-06 09:32:51 +0900

    feat: SubjectController

---
+++ src/main/kotlin/com/example/hackaton16/domain/subject/presentation/SubjectController.kt
@@ -0,0 +1,33 @@
+package com.example.hackaton16.domain.subject.presentation
+
+import com.example.hackaton16.domain.subject.presentation.dto.response.QuerySubjectListResponse
+import com.example.hackaton16.domain.subject.service.QuerySubjectListService
+import com.example.hackaton16.domain.subject.service.SaveSubjectService
+import org.springframework.http.HttpStatus
+import org.springframework.http.MediaType
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestPart
+import org.springframework.web.bind.annotation.ResponseStatus
+import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.multipart.MultipartFile
+
+@RestController
+@RequestMapping("/subject")
+class SubjectController(
+    private val saveSubjectService: SaveSubjectService,
+    private val querySubjectListService: QuerySubjectListService
+) {
+    @PostMapping(consumes = [MediaType.MULTIPART_FORM_DATA_VALUE])
+    @ResponseStatus(HttpStatus.CREATED)
+    fun saveSubject(@RequestPart("image") file: MultipartFile) {
+        saveSubjectService.execute(file)
+    }
+
+    @GetMapping
+    @ResponseStatus(HttpStatus.OK)
+    fun querySubjectList(): QuerySubjectListResponse {
+        return querySubjectListService.execute()
+    }
+}
