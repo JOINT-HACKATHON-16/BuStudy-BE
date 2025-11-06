24c7648a0f15b989bfa061577a79d654a8f47213
coehgns <modooboiroo@gmail.com>
2025-11-06 09:12:48 +0900

    feat: SubjectRepository에 findAllByUser 추가

--- src/main/kotlin/com/example/hackaton16/domain/subject/domain/repository/SubjectRepository.kt
+++ src/main/kotlin/com/example/hackaton16/domain/subject/domain/repository/SubjectRepository.kt
@@ -1,7 +1,10 @@
 package com.example.hackaton16.domain.subject.domain.repository
 
 import com.example.hackaton16.domain.subject.domain.Subject
+import com.example.hackaton16.domain.user.domain.User
 import org.springframework.data.jpa.repository.JpaRepository
 
 interface SubjectRepository : JpaRepository<Subject, Long> {
+
+    fun findAllByUser(user: User): List<Subject>?
 }
