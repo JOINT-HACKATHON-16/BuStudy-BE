3dd241934494152f4c4ed589cbbd0e707b95074b
coehgns <modooboiroo@gmail.com>
2025-11-06 23:41:49 +0900

    refactor: GenerateEstimatedTimeService

--- src/main/kotlin/com/example/hackaton16/domain/bus/service/GenerateEstimatedTimeService.kt
+++ src/main/kotlin/com/example/hackaton16/domain/bus/service/GenerateEstimatedTimeService.kt
@@ -2,22 +2,20 @@ package com.example.hackaton16.domain.bus.service
 
 import com.example.hackaton16.domain.bus.presentation.dto.request.GenerateEstimatedTimeRequest
 import com.example.hackaton16.domain.bus.presentation.dto.response.GenerateEstimatedTimeResponse
+import com.example.hackaton16.infrastructure.feign.client.bus.BusClient
+import com.example.hackaton16.infrastructure.feign.client.bus.dto.request.BusEstimatedTimeRequest
 import com.example.hackaton16.infrastructure.feign.client.kakao.KakaoClient
-import com.example.hackaton16.infrastructure.feign.client.odsay.OdSayClient
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Service
 
 @Service
 class GenerateEstimatedTimeService(
     private val kakaoClient: KakaoClient,
-    private val odSayClient: OdSayClient
+    private val busClient: BusClient
 ) {
 
     @Value("\${app.kakao-map-api.api-key}")
     lateinit var kakaoApiKey: String
-
-    @Value("\${app.od-say.api-key}")
-    lateinit var odSayApiKey: String
 
     fun execute(request: GenerateEstimatedTimeRequest): GenerateEstimatedTimeResponse {
         val coordinate = kakaoClient.getLocationInfo(
@@ -30,14 +28,15 @@ class GenerateEstimatedTimeService(
         val arrivalY = coordinate.first
         val arrivalX = coordinate.second
 
-        val estimatedTime = odSayClient.searchPubTransPathT(
-            odSayApiKey,
-            request.sx,
-            request.sy,
-            arrivalX,
-            arrivalY
+        val generateEstimatedTimeResponse = busClient.generateEstimatedTime(
+            BusEstimatedTimeRequest(
+                request.sx,
+                request.sy,
+                arrivalX,
+                arrivalY
+            )
         )
 
-        return GenerateEstimatedTimeResponse(estimatedTime.totalTime)
+        return GenerateEstimatedTimeResponse(generateEstimatedTimeResponse.estimatedTime)
     }
 }
