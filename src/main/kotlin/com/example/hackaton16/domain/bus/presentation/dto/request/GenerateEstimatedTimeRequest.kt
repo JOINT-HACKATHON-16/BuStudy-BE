4834a3330ddbc22ccb883bef64de168f4306e37e
coehgns <modooboiroo@gmail.com>
2025-11-06 17:57:25 +0900

    refactor: GenerateEstimatedTimeRequest 좌표 nullable하게 설정

--- src/main/kotlin/com/example/hackaton16/domain/bus/presentation/dto/request/GenerateEstimatedTimeRequest.kt
+++ src/main/kotlin/com/example/hackaton16/domain/bus/presentation/dto/request/GenerateEstimatedTimeRequest.kt
@@ -10,12 +10,12 @@ data class GenerateEstimatedTimeRequest(
     @field:NotNull(message = "sx(경도)는 필수 값입니다.")
     @field:DecimalMin(value = "-180.0", message = "sx(경도)는 -180 이상이어야 합니다.")
     @field:DecimalMax(value = "180.0", message = "sx(경도)는 180 이하여야 합니다.")
-    val sx: Double?,
+    val sx: Double,
 
     @field:NotNull(message = "sy(위도)는 필수 값입니다.")
     @field:DecimalMin(value = "-90.0", message = "sy(위도)는 -90 이상이어야 합니다.")
     @field:DecimalMax(value = "90.0", message = "sy(위도)는 90 이하여야 합니다.")
-    val sy: Double?,
+    val sy: Double,
 
     @field:NotBlank(message = "도로명 주소는 필수 값입니다.")
     @field:Size(max = 255, message = "도로명 주소는 255자 이하로 입력해주세요.")
