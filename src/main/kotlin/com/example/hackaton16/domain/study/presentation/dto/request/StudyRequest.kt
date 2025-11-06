6b0de286bcca9221c204c1cf6689465548e5dd10
coehgns <modooboiroo@gmail.com>
2025-11-06 17:27:15 +0900

    feat: StudyRequest validation 적용

--- src/main/kotlin/com/example/hackaton16/domain/study/presentation/dto/request/StudyRequest.kt
+++ src/main/kotlin/com/example/hackaton16/domain/study/presentation/dto/request/StudyRequest.kt
@@ -1,9 +1,11 @@
 package com.example.hackaton16.domain.study.presentation.dto.request
 
+import jakarta.validation.constraints.NotNull
 import jakarta.validation.constraints.Size
 
 data class StudyRequest(
 
     @field:Size(min = 1, message = "예상 시간은 최소 1분 이상이여야 합니다.")
+    @field:NotNull(message = "예상 시간은 null일 수 없습니다.")
     val estimatedTime: Int
 )
