d49d1354993cd025436540de7f0f49e1fd52e760
coehgns <modooboiroo@gmail.com>
2025-11-06 13:53:49 +0900

    feat: SubjectRepository에 findSubjectById 추가

--- src/main/kotlin/com/example/hackaton16/domain/subject/domain/repository/SubjectRepository.kt
+++ src/main/kotlin/com/example/hackaton16/domain/subject/domain/repository/SubjectRepository.kt
@@ -7,4 +7,6 @@ import org.springframework.data.jpa.repository.JpaRepository
 interface SubjectRepository : JpaRepository<Subject, Long> {
 
     fun findAllByUser(user: User): List<Subject>?
+
+    fun findSubjectById(subjectId: Long): Subject?
 }
