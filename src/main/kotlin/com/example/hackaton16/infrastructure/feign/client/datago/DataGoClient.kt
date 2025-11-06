14c72d357f8a53e253efb5418f206c5dadede685
coehgns <modooboiroo@gmail.com>
2025-11-06 13:34:03 +0900

    feat: DataGoClient

---
+++ src/main/kotlin/com/example/hackaton16/infrastructure/feign/client/datago/DataGoClient.kt
@@ -0,0 +1,20 @@
+package com.example.hackaton16.infrastructure.feign.client.datago
+
+import com.example.hackaton16.infrastructure.feign.client.datago.dto.response.BusStationResponse
+import org.springframework.cloud.openfeign.FeignClient
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestParam
+
+@FeignClient(
+    name = "DataGoClient",
+    url = "\${app.data-go.base-url}"
+)
+interface DataGoClient {
+    @GetMapping("getCrdntPrxmtSttnList")
+    fun getBusStopList(
+        @RequestParam("serviceKey") serviceKey: String,
+        @RequestParam("gpsLati") gpsLati: Double,  // GPS Y좌표
+        @RequestParam("gpsLong") gpsLong: Double,  // GPS X좌표
+        @RequestParam("_type") type: String? = "json"
+    ): BusStationResponse
+}
