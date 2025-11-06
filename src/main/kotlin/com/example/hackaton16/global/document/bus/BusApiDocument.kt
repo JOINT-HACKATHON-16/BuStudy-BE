5a41269e9711f55cd6c1d37ade7ab3997119c32a
coehgns <modooboiroo@gmail.com>
2025-11-06 14:42:02 +0900

    feat: BusApiDocument

--- src/main/kotlin/com/example/hackaton16/global/document/bus/BusApiDocument.kt
+++ src/main/kotlin/com/example/hackaton16/global/document/bus/BusApiDocument.kt
@@ -1,10 +1,13 @@
 package com.example.hackaton16.global.document.bus
 
+import com.example.hackaton16.domain.bus.presentation.dto.request.GenerateEstimatedTimeRequest
+import com.example.hackaton16.domain.bus.presentation.dto.response.GenerateEstimatedTimeResponse
 import com.example.hackaton16.global.error.ErrorResponse
 import com.example.hackaton16.infrastructure.feign.client.datago.dto.response.BusStopResponse
 import io.swagger.v3.oas.annotations.Operation
 import io.swagger.v3.oas.annotations.media.Content
 import io.swagger.v3.oas.annotations.media.Schema
+import io.swagger.v3.oas.annotations.parameters.RequestBody
 import io.swagger.v3.oas.annotations.responses.ApiResponse
 import io.swagger.v3.oas.annotations.responses.ApiResponses
 import io.swagger.v3.oas.annotations.tags.Tag
@@ -32,4 +35,21 @@ interface BusApiDocument {
         @RequestParam lat: Double,
         @RequestParam lon: Double
     ): List<BusStopResponse.BusStationItem>
+
+    @Operation(summary = "버스 이동 예상 시간")
+    @ApiResponses(
+        value = [
+            ApiResponse(
+                responseCode = "200",
+                description = "버스 이동 예상 시간 조회 성공",
+                content = [Content(schema = Schema(implementation = GenerateEstimatedTimeResponse::class))]
+            ),
+            ApiResponse(
+                responseCode = "400",
+                description = "잘못된 요청",
+                content = [Content(schema = Schema(implementation = ErrorResponse::class))]
+            )
+        ]
+    )
+    fun generateEstimatedTime(@RequestBody request: GenerateEstimatedTimeRequest): GenerateEstimatedTimeResponse
 }
