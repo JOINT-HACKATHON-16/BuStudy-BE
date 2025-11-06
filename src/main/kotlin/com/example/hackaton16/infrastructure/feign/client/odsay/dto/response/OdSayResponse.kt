953c3cb510d15f2cafaf4b06a0c8f450b3731bdc
coehgns <modooboiroo@gmail.com>
2025-11-06 23:41:30 +0900

    chore: Odsay 삭제

--- src/main/kotlin/com/example/hackaton16/infrastructure/feign/client/odsay/dto/response/OdSayResponse.kt
+++
@@ -1,8 +0,0 @@
-package com.example.hackaton16.infrastructure.feign.client.odsay.dto.response
-
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties
-
-@JsonIgnoreProperties(ignoreUnknown = true)
-data class OdSayResponse(
-    val totalTime: Int
-)
