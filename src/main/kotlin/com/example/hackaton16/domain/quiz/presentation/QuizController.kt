b4e444f2c07ec2fd6c07bddd236cc16a49ec839c
coehgns <modooboiroo@gmail.com>
2025-11-06 23:13:15 +0900

    feat: QuizApiDocument 적용

--- src/main/kotlin/com/example/hackaton16/domain/quiz/presentation/QuizController.kt
+++ src/main/kotlin/com/example/hackaton16/domain/quiz/presentation/QuizController.kt
@@ -3,23 +3,21 @@ package com.example.hackaton16.domain.quiz.presentation
 import com.example.hackaton16.domain.quiz.presentation.dto.request.MarkQuizRequest
 import com.example.hackaton16.domain.quiz.presentation.dto.response.MarkQuizResponse
 import com.example.hackaton16.domain.quiz.service.MarkQuizService
+import com.example.hackaton16.global.document.quiz.QuizApiDocument
 import jakarta.validation.Valid
-import org.springframework.http.HttpStatus
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.ResponseStatus
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @RequestMapping("/quiz")
 class QuizController(
     private val markQuizService: MarkQuizService
-) {
+) : QuizApiDocument {
 
     @PostMapping
-    @ResponseStatus(HttpStatus.OK)
-    fun markQuiz(@RequestBody @Valid request: MarkQuizRequest): MarkQuizResponse {
+    override fun markQuiz(@RequestBody @Valid request: MarkQuizRequest): MarkQuizResponse {
         return markQuizService.execute(request)
     }
 }
